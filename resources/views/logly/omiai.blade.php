a7e11f886851b98a331b7aa7fd3215cc0eeed125
EC2 Default User <ec2-user@ip-172-31-46-119.us-east-2.compute.internal>
2020-09-09 23:45:02 +0000

    mM

---
+++ resources/views/logly/omiai.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>テスト</title>
+        <link href="{{ secure_asset('css/omiai.css') }}" rel="stylesheet">
+        <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <!--LP名-->
+            <h3><span class="header-span">LEMON.</span></h3>
+        </header>
+        
+        <div class="body">
+            <div class="title">
+                <!--タイトル-->
+                <h3>【画像あり】37歳おっさんが『真剣に』婚活アプリを3ヶ月使った成果報告！！</h3>
+                <!--日付-->
+                <p>2020-09-11 18:37</p>
+                <!--画像-->
+                <img src="{{ asset('image/lp_omi_1.jpg') }}"></img>
+            </div>
+            <div class="button">
+                
+            </div>
+            <div class="index">
+                <!--目次-->
+            </div>
+            
+        </div>
+        
+        <footer>
+            
+        </footer>
+    </body>
+</html>
