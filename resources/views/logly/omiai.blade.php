f63c0c74d23adf1fe072c2984b2f3ad70469667a
EC2 Default User <ec2-user@ip-172-31-46-119.us-east-2.compute.internal>
2020-09-11 18:47:52 +0000

    Mm

--- resources/views/logly/omiai.blade.php
+++ resources/views/logly/omiai.blade.php
@@ -154,10 +154,12 @@
                 </div><!--/①LINE会話終了-->
                 
                 <h5>と、言うことで...<br><br><span class="content-h5-span">早速始めてみることに！(泣)</span></h5>
-                <img src="{{ asset('image/lp_omi_4.jpg') }}" class="width10"></img>
-                <img src="{{ asset('image/lp_omi_5.jpg') }}" class="width30"></img>
-                <img src="{{ asset('image/lp_omi_6.jpg') }}" class="width50"></img>
-                <img src="{{ asset('image/lp_omi_7.jpg') }}" class="width100"></img>
+                <div class="center">
+                    <img src="{{ asset('image/lp_omi_4.jpg') }}" class="width10"></img>
+                    <img src="{{ asset('image/lp_omi_5.jpg') }}" class="width30"></img>
+                    <img src="{{ asset('image/lp_omi_6.jpg') }}" class="width50"></img>
+                    <img src="{{ asset('image/lp_omi_7.jpg') }}" class="width100"></img>
+                </div>
             </div>
         </div>
         
